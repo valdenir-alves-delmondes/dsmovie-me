dd0143f03675a3397343e0202da577c347f8b2bf
valdenir-alves-delmondes <valdeniralvesdelmondes@gmail.com>
2022-05-15 16:13:50 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperior/dsmovieme/DsmovieMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovieme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
